84ebecd69bfeac2c0cbe7149e0e4279d6014884a
satyan@ <irinasatyan88@gmail.com>
2021-11-28 17:56:56 +0400

    week2 classwork

---
+++ src/week2/task1.java
@@ -0,0 +1,12 @@
+package week2;
+
+Scanner scanner = new Scanner(System.in);
+
+        byte b = scanner.nextByte();
+        short s = b;
+        System.out.println(b);
+        System.out.println(s);
+        int(x)=scunner.nextInt()
+
+}
+}
